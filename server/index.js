888def93589149fd52c5905c7f7d5230f349e90f
hayderabdela <hayderabdela@1112gmail.com>
2023-12-15 21:55:59 +0300

    deploy website

--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const cloudinary = require('cloudinary').v2;
 const auth = require('./routes/authRouter')
-const cloudinaryRouter = require('./routes/cloudinaryRouter')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -19,16 +17,9 @@ mongoose
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-// Cloudinary Configuration
-// cloudinary.config({
-//   cloud_name: 'dql9jy5wl',
-//   api_key: '196365693146448',
-//   api_secret: 'yEk7IlaYNQ8gYMhENp-d4b02MEM',
-// });
+
 // Routes
 app.use('/auth', auth);
-// Cloudinary Configuration
-app.use('/cloudinary', cloudinaryRouter); 
 
 // Start the server
 app.listen(PORT, () => {
